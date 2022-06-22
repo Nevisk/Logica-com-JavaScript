ba4ce9b666af085d50dd29a8a61c8cbb306651f8
brunogodoy <brunogodoy016@gmail.com>
2022-06-22 11:14:30 -0300

    Primeiro-commit

---
+++ Capitulo 1/exemplo-1.6.js
@@ -0,0 +1,20 @@
+//calculo dobro de um numero
+
+/* 
+A função abaixo calcula apenas operações com multiplicação, divisão ou subtração.  
+*/
+var num1 =  prompt("Insira um número:");
+
+var dobro = num1 * 2;
+
+alert("O dobro do numero é " + dobro);
+
+/* 
+Já para realizar operações de soma é necessário fazer uma conversão, da entrada de dados para o tipo Number 
+*/
+var num1 =  Number(prompt("Insira um número para somar com 10:"));
+
+var soma = num1 + 10;
+
+alert("A soma do numero é " + soma);
+
---
+++ Home/script.js
@@ -0,0 +1,9 @@
+/* 
+Função simples de entrada e saida de dados com interação do usuário. 
+*/
+
+var nome = prompt("Olá. Qual o seu nome? ");
+
+alert("Seja bem vindo(a), " +nome+ ".");
+
+//Fim da função
